964dbc909391b486305cd480dba1893bde3f9ecd
macbook <macbook@macbookdeMacBook-Air.local>
2017-05-25 12:14:44 +0800

    初始提交

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by macbook on 2017/5/22.
+//  Copyright © 2017年 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by macbook on 2017/5/22.
+//  Copyright © 2017年 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
